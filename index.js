dc84527723083709aa12b674c7e7e72f30fe2623
Jawwad Umar <you@example.com>
2025-05-17 15:57:03 +0530

    importing exporting multiple functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const generateRandomNumbers = require('./utils')
+const {generateRandomNumbers, degreeToFahrenheit} = require('./utils')
 
 console.log(generateRandomNumbers());
+console.log(degreeToFahrenheit(100));
--- utils.js
+++ utils.js
@@ -3,4 +3,10 @@ const generateRandomNumbers = () =>{
     return Math.floor(Math.random()*100) + 1;
 }
 
-module.exports = generateRandomNumbers;
+
+const degreeToFahrenheit = (degree) =>{
+
+    return 9/5 *degree + 32;
+}
+
+module.exports = {generateRandomNumbers, degreeToFahrenheit};
